729b55b2cb46c98c3064ffedf6bd0a2d3e33e9b2
simona <palumbosimona118@gmail.com>
2019-12-17 21:25:35 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nomeUtente = prompt('il tuo nome');
+console.log(nomeUtente);
+var cognomeUtente = prompt('il tuo cognome');
+console.log(cognomeUtente);
+var colorePreferito = prompt('il tuo colore preferito');
+console.log(colorePreferito);
+var passwordGenerator = 'simonapalumboverde19';
+document.getElementById('password-generator').innerHTML = passwordGenerator; 
